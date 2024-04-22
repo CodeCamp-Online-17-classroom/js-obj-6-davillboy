1e68c8637b6529e20c45661b19ad50281fb5e9bc
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 15:16:52 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // write code here
+<script>
+        function isEmpty(obj) {
+            for (let key in obj) {
+              
+              if (obj.hasOwnProperty(key)) {
+                return false;
+              }
+            }
+           
+            return true;
+          }
+        
+          let emptyObj = {};
+          let objWithProperties = { key: 'value' };
+          
+          console.log(isEmpty(emptyObj));
+          console.log(isEmpty(objWithProperties)); 
+          
+    </script>
